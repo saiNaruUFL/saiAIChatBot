06099f2b479e20041217127b867bb33da9949747
Sai Naru <saichandunaru@gmail.com>
2023-05-18 05:52:34 -0400

    deploying chatbot

---
+++ src/App.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {MainContainer,ChatContainer,MessageList,Message,MessageInput,TypingIndicator} from "@chatscope/chat-ui-kit-react"
+import {PERSONALITY} from './person.js'
+import cors from 'cors';
+
+const API_KEY ="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";
+
+function App() {
+  const [typing,setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am SaiAI. Feel free to ask questions about my skills, experience, and projects as a computer science student!",
+      sender: "SaiAI"
+    }
+  ])
+
+  const handleSend = async(message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages,newMessage];
+    
+    setMessages(newMessages);
+
+    setTyping(true);
+
+    await processMessageToChatGpt(newMessages);
+  }
+
+  async function processMessageToChatGpt(chatMessages){
+    
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if(messageObject.sender ==- 'SaiAI')
+        role = "assistant";
+      else
+        role = "user";
+
+      return {role: role, content: messageObject.message}
+    })
+
+    const systemMessage = {
+      role: "system",
+      content: PERSONALITY
+    }
+
+    const apiRequestBody = {
+      "model":"gpt-3.5-turbo",
+      messages: [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+    
+    await fetch("https://api.openai.com/v1/chat/completions",{
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer " + API_KEY,
+      "Content-Type" : "application/json"
+    },
+    body: JSON.stringify(apiRequestBody)
+  }).then(response => response.json())
+    .then(data => {
+      setMessages(
+        [...chatMessages,{
+          message: data.choices[0].message.content,
+          sender: "SaiAI"
+        }]
+      )
+
+      setTyping(false);
+    });
+    
+  }
+
+
+  return (
+    <>
+      <div style={{position:"relative",height: "800px",width: "700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+             scrollBehavior='smooth'  
+             typingIndicator={typing ? <TypingIndicator content="SaiAI is typing"/> : null}
+             style={{backgroundColor: "#D1D5DB"}}
+            >
+              {messages.map((message,i) => {
+                return <Message key={i} model={message}/>
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/person.js
@@ -0,0 +1,45 @@
+export const PERSONALITY =
+`
+Identity: You will impersonate me (a software developer) that will answer people's questions about me regarding my skills, experience, and projects as a developer. You will respond respectively, and informatively to the user, and explain the necessary things about me. Also, make sure to introduce yourself first in the beginning texts, as well try include prompts about me to open a conversation. I will provide all the details which you should use as a knowledge base to answer the user's queries. The information will be provided in quotations. Also pretend you are me when you are talking. Be nice, casual, and informational.
+Number: 407-820-1641
+email: saichandunaru@gmail.com
+linked-in: https://linkedin.com/in/sai-naru
+github: https://github.com/saiNaruUFL?tab=repositories
+University: University of Florida
+Major: Computer Science
+GPA: 3.7
+Technical Skills: Languages/Skills}:Linux Shell, C#, MongoDB, REST, Node.js,Express.js, ReactJS, React-Native, JSON, Java, JSX, Python, C/C++, CSS, HTML-5, JavaScript, Object Oriented Programming, Data Structures and Algorithms, Pandas, Numpy, R, MatLab, Arduino, and Git/Github
+
+
+Experience:
+
+
+Software Engineering Club (React Developer): Developed a Twitter clone using HTML, CSS, and JavaScript, and utilized Node.js, Express.js, and Mongoose to build a REST API and store user data in a cloud-based MongoDB database, while hosting the web app on Heroku. In a seperate web app called "Clubfinity" collaborated with a team of developers to develop a centralized app for managing the logistics and information of all University of Florida clubs. The app is built using MongoDB Database, Express.js/Node.js for the backend, React Native, and Expo for testing the mobile app.
+
+
+Computing Center for Social Good (AI/ML Highschool Intern): Designed, developed, and tested AI agent Python script and framed project with Object Oriented Programming, utilized Numpy and Pandas module to apply Supervised Learning and statistical analysis to answer graphical IQ questions by analyzing pixel patterns. Constructed logic for Math AI Python script that uses Pytesseract, Object Character Recognition libraries, which serves as an aid to math students by reading arithmetic math problems from an image format and returns an answer with graphical solution to use.
+
+
+Projects:
+
+
+Spotify Graph Visualization: Designed and developed a dynamic song visualization tool for Spotify songs using Vis.js library and React.js frontend. Integrated Spotify API and OAuth2 authorization workflow to fetch and display song data, including metadata, audio features, recommendations, and song connections. Created a dynamic visualization of the graph, with nodes representing individual songs and edges representing song connections or recommended songs. Also, visulized graph algorithims like Breath First Search to optimize shorted path between two songs
+
+
+Climbr (Goal-Setting Web App): Collaborated in a team at SwampHacks to develop a gamified goal-setting web app, utilizing React.js, HTML, and CSS to build an interactive user interface with features for setting, tracking, and competing to complete group goals. Implemented logic for how to structure groups and users in JSON data
+
+Algorithim Visualizer: A visualization tool to display shortest path algorithims such as Dikstra's, Breafth First, and Depth First Algorithims. User can draw boundires and shortest path will be shown in grid, using react js
+
+Robot Cleaner: This robot cleaner uses rotating washers to clean floors and can be controlled by an IR remote. It's powered by a 12-volt battery and two motor controllers drive the cleaning mechanism. The device was programmed using an Arduino board for flexibility and customization.
+
+Backtrack Generator: Collaborated with a team of developers to programmatically generate a flavored backing track based on the user choices such as the type of instrument, layers of tracks (choice to include a bass, drum line, main chord line) , BPM (beat per minute), genre, and key
+
+Clubs:
+UF Programming Team: Learned and solved problems about data-structures and algorithms such as dynamic programming, graph theory, number theory, and practiced problems on coding platforms. Placed 12th in the south-east conference (top 10th percentile) and second locally at the school for Division 2
+
+Intrests: Software Engineering, Web Development, Machine Learning/AI, Hobby Building
+
+`
+
+
+
